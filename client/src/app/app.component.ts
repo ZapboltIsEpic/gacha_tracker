5fba947989b5c47a07b7f793675602ee12585e89
Yijun Zou <yijun.zou@student.unsw.edu.au>
2024-10-01 16:33:39 +1000

    added some basic stuff after learning more angular

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   template: `
+    <app-navbar></app-navbar>
     <h1>Welcome to {{title}}!</h1>
-
+      <span>
+        @for (game of games; track game.id) {
+          {{game.name}}
+        }
+      </span>
     <router-outlet />
   `,
   styles: [],
 })
 export class AppComponent {
   title = 'Gacha Tracker';
+  games = [{id: 0, name: 'genshin-impact'}, {id: 1, name: 'arknights'}];
 }
