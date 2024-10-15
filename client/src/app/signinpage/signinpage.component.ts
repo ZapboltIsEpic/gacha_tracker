263ca4d2b737be731937fc64c220947a3f9de803
Yijun Zou <zapboltisepic@gmail.com>
2024-10-15 15:53:06 +1100

    added simple email and password login

--- client/src/app/signinpage/signinpage.component.ts
+++ client/src/app/signinpage/signinpage.component.ts
@@ -1,16 +1,52 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
 
 @Component({
   selector: 'app-signinpage',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   template: `
-    <p>
-      signinpage works!
-    </p>
+    <form [formGroup]="applyForm" (ngSubmit)="onSubmit()">
+      <label>
+        Email:
+        <input id="email" type="email" formControlName="email">
+      </label>
+      <label>
+        Password:
+        <input id="password" type="password" formControlName="password">
+      </label>
+      <button type="submit">Submit</button>
+    </form>
   `,
   styles: ``
 })
 export class SigninpageComponent {
+  // @Injectable({
+  //   providedIn: 'root'
+  // })
+  // export class SigninService {
+  //   constructor(private http: HttpClient) {}
 
+  //   signin(email: string, password: string) {
+  //     return this.http.get(`/api/signin?email=${email}&password=${password}`);
+  //   }
+  // }
+
+  // constructor(private signinService: SigninService) {}
+
+  // onSubmit() {
+  //   const { email, password } = this.applyForm.value;
+  //   this.signinService.signin(email, password).subscribe(response => {
+  //     console.log('Response from backend:', response);
+  //   });
+  // }
+  applyForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
+  onSubmit() {
+    console.log(this.applyForm.value);
+  }
 }
