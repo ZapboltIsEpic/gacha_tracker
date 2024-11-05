3de7e45e4d8ee1942c2295968cfaf49fce145de3
Yijun Zou <zapboltisepic@gmail.com>
2024-11-05 16:18:39 +1100

    fixed hydration error

--- client/src/app/pages/fategrandorder/servants/servants.component.ts
+++ client/src/app/pages/fategrandorder/servants/servants.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { LocalStorageService } from '../../../local-storage.service';
@@ -10,11 +10,11 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <div [class]="homepage">
-      <div>
+      <div *ngIf="personalisedServant===''">
         <h1>Servants</h1>
         <table>
-          <tr *ngFor="let servant of servants">
-            <td [class]="servantBox">
+          <tr *ngFor="let servant of servants; trackBy: trackByServant">
+            <td [class]="servantBox" (click)="getPersonalisedServant(servant.name)">
               <img src="{{ servant.image }}" height = 222px width =130px>
             </td>
             <td>
@@ -23,17 +23,33 @@ import { CommonModule } from '@angular/common';
           </tr>
         </table>
       </div>
+      <div *ngIf="personalisedServant!=''">
+        <div>
+          personalised servant: {{ personalisedServant }}
+        </div>
+        <button (click)="getPersonalisedServant('')">Back</button>
+      </div>
     </div>
   `,
-  styleUrls: ['./servants.component.css', '../fategrandorderhomepage/fategrandorderhomepage.component.css']
+  styleUrls: ['./servants.component.css', '../fategrandorderhomepage/fategrandorderhomepage.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ServantsComponent {
-  servantBox = 'servantBox';
-  // add personalised servants and all servants in different tabs
-  constructor(private router: Router, private http: HttpClient, private localStorageService : LocalStorageService) {}
-
   homepage = 'homepage';
   servants: any[] = [];
+  servantBox = 'servantBox';
+  personalisedServant = '';
+  trackByServant(index: number, servant: any): string {
+    return servant.name;
+  }
+
+  getPersonalisedServant(servant: string): void {
+    console.log('Personalised servant:', servant);
+    this.personalisedServant = servant;
+  }
+
+  // add personalised servants 
+  constructor(private router: Router, private http: HttpClient, private localStorageService : LocalStorageService) {}
 
   ngOnInit(): void {
     this.getServants();
