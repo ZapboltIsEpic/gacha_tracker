7e4342d34138b0f53367fd46cc33fa519f02a77d
Yijun Zou <zapboltisepic@gmail.com>
2024-11-12 16:31:31 +1100

    added class filtering for servants

--- client/src/app/pages/fategrandorder/servants/servants.component.ts
+++ client/src/app/pages/fategrandorder/servants/servants.component.ts
@@ -16,7 +16,10 @@ import { ChangeDetectorRef } from '@angular/core';
       <div *ngIf="personalisedServant===''">
         <h1>Servants</h1>
         <div [class]="row">
-          <span><img alt="saber class icon" title="Saber" src="https://static.atlasacademy.io/JP/ClassIcons/class2_1.png" width="37" height="37"></span>
+          <span *ngFor="let servant of servantIcons" [class]="filter" (click)="filterByClass(servant.title)">
+            <img *ngIf="servantFilterClassMap[servant.title]" [alt]="servant.name" [title]="servant.title" [src]="servant.image2" width="37" height="37">
+            <img *ngIf="!servantFilterClassMap[servant.title]" [alt]="servant.name" [title]="servant.title" [src]="servant.image" width="37" height="37">
+          </span>
         </div>
         <app-searchbar></app-searchbar>
         <table>
@@ -32,7 +35,7 @@ import { ChangeDetectorRef } from '@angular/core';
             <tr *ngIf="!servants || !servants.length">
               <td colspan="4">Loading...</td>
             </tr>
-            <tr *ngFor="let servant of servants; trackBy: trackByServant">
+            <tr *ngFor="let servant of filteredServants; trackBy: trackByServant" >
               <td>{{servant.class}}</td>
               <td (click)="getPersonalisedServant(servant.name)">
                 <img src="{{ servant.image }}" height = 222px width =130px>
@@ -59,11 +62,40 @@ import { ChangeDetectorRef } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ServantsComponent {
+  filter = 'filter';
   row = 'row';
   homepage = 'homepage';
   servants: any[] = [];
+  filteredServants: any[] = [];
   servantBox = 'servantBox';
   personalisedServant = '';
+
+  servantFilterClassMap : { [key : string]: boolean } = {
+    "All" : true,
+    "Saber" : false,
+    "Archer" : false,
+    "Lancer" : false,
+    "Rider" : false,
+    "Caster" : false,
+    "Assassin" : false,
+    "Berserker" : false,
+    "Ruler" : false,
+    "Avenger" : false,
+    "Moon Cancer" : false,
+    "Alter Ego" : false,
+    "Foreigner" : false,
+    "Pretender" : false,
+    "Unknown" : false
+  };
+
+  servantFilterRarityMap: { [key: number]: boolean } = {
+    0 : true,
+    1: false,
+    2: false,
+    3: false,
+    4: false,
+    5: false
+  };
   
   trackByServant(index: number, servant: any): string {
     return servant.name;
@@ -79,6 +111,7 @@ export class ServantsComponent {
 
   ngOnInit(): void {
     this.getServants();
+    this.getFilteredServants();
   }
 
   getServants(): void {
@@ -91,4 +124,49 @@ export class ServantsComponent {
         console.error('Error fetching servants:', error);
       });
   }
+
+  getFilteredServants() : void {
+    this.filteredServants = [];
+    this.servants.forEach((servant: any) => {
+      const { class: servantClass, rarity } = servant;
+      const push_filter = (this.servantFilterClassMap["All"] && this.servantFilterRarityMap[0]) || 
+                          (this.servantFilterClassMap["All"] && this.servantFilterRarityMap[rarity]) || 
+                          (this.servantFilterClassMap[servantClass] && this.servantFilterRarityMap[0]) ||
+                          (this.servantFilterClassMap[servantClass] && this.servantFilterRarityMap[rarity]);
+      
+      if (push_filter) {
+        this.filteredServants.push(servant);
+      }
+    });
+    console.log('Filtered Servants', this.filteredServants);
+    this.cd.detectChanges();
+  }
+
+  filterByClass(servantClass : string) {
+    this.servantFilterClassMap[servantClass] = !this.servantFilterClassMap[servantClass];
+    this.checkFilteredServants();
+    this.getFilteredServants();
+  }
+
+  checkFilteredServants() {
+    const anyOtherFilterActive = Object.entries(this.servantFilterClassMap).some(([key, value]) => key !== "All" && value);
+    this.servantFilterClassMap["All"] = !anyOtherFilterActive;
+  }
+
+  servantIcons = [
+    { name: "saber class icon", title: "Saber", image: "https://static.atlasacademy.io/JP/ClassIcons/class2_1.png", image2: "https://static.atlasacademy.io/JP/ClassIcons/class3_1.png"},
+    { name: "archer class icon", title: "Archer", image: "https://static.atlasacademy.io/JP/ClassIcons/class2_2.png", image2: "https://static.atlasacademy.io/JP/ClassIcons/class3_2.png" },
+    { name: "lancer class icon", title: "Lancer", image: "https://static.atlasacademy.io/JP/ClassIcons/class2_3.png", image2: "https://static.atlasacademy.io/JP/ClassIcons/class3_3.png" },
+    { name: "rider class icon", title: "Rider", image: "https://static.atlasacademy.io/JP/ClassIcons/class2_4.png", image2: "https://static.atlasacademy.io/JP/ClassIcons/class3_4.png" },
+    { name: "caster class icon", title: "Caster", image: "https://static.atlasacademy.io/JP/ClassIcons/class2_5.png", image2: "https://static.atlasacademy.io/JP/ClassIcons/class3_5.png" },
+    { name: "assassin class icon", title: "Assassin", image: "https://static.atlasacademy.io/JP/ClassIcons/class2_6.png", image2: "https://static.atlasacademy.io/JP/ClassIcons/class3_6.png" },
+    { name: "berserker class icon", title: "Berserker", image: "https://static.atlasacademy.io/JP/ClassIcons/class2_7.png", image2: "https://static.atlasacademy.io/JP/ClassIcons/class3_7.png" },
+    { name: "ruler class icon", title: "Ruler", image: "https://static.atlasacademy.io/JP/ClassIcons/class2_9.png", image2: "https://static.atlasacademy.io/JP/ClassIcons/class3_9.png" },
+    { name: "avenger class icon", title: "Avenger", image: "https://static.atlasacademy.io/JP/ClassIcons/class2_11.png", image2: "https://static.atlasacademy.io/JP/ClassIcons/class3_11.png" },
+    { name: "moonCancer class icon", title: "Moon Cancer", image: "https://static.atlasacademy.io/JP/ClassIcons/class2_23.png", image2: "https://static.atlasacademy.io/JP/ClassIcons/class3_23.png" },
+    { name: "alterEgo class icon", title: "Alter Ego", image: "https://static.atlasacademy.io/JP/ClassIcons/class2_10.png", image2: "https://static.atlasacademy.io/JP/ClassIcons/class3_10.png" },
+    { name: "foreigner class icon", title: "Foreigner", image: "https://static.atlasacademy.io/JP/ClassIcons/class2_25.png", image2: "https://static.atlasacademy.io/JP/ClassIcons/class3_25.png" },
+    { name: "pretender class icon", title: "Pretender", image: "https://static.atlasacademy.io/JP/ClassIcons/class2_28.png", image2: "https://static.atlasacademy.io/JP/ClassIcons/class3_28.png" },
+    { name: "unknown class icon", title: "Unknown", image: "https://static.atlasacademy.io/JP/ClassIcons/class2_97.png", image2: "https://static.atlasacademy.io/JP/ClassIcons/class3_97.png" }
+  ];
 }
